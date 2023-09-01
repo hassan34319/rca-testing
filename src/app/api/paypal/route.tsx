517a35a29169d88ba89fdba5f67d74dc1e3eb9bb
hassan34319 <mh08062@st.habib.edu.pk>
2023-09-01 11:19:59 +0500

    fixed

--- src/app/api/paypal/route.tsx
+++ src/app/api/paypal/route.tsx
@@ -1,51 +1,49 @@
-import client from 'backend/paypal'
+import { NextResponse } from 'next/server'
 import paypal from '@paypal/checkout-server-sdk'
 
+import client from '../../utils/paypal/index'
 
 
-export default async function Handler(req, res) {
 
-  if(req.method != "POST")
-    return res.status(404).json({success: false, message: "Not Found"})
+export async function POST(request: Request) {
+  const body = await request.json()
 
-  if(!req.body.order_price || !req.body.user_id)
-    return res.status(400).json({success: false, message: "Please Provide order_price And User ID"})
+
+  if(body.order_price || !body.user_id)
+  return NextResponse.json({ success: false, error: "Please provide order Id and Price" })
 
 
   try{
     const PaypalClient = client()
-    //This code is lifted from https://github.com/paypal/Checkout-NodeJS-SDK
-    const request = new paypal.orders.OrdersCreateRequest()
-    request.headers['prefer'] = 'return=representation'
-    request.requestBody({
+    // This code is lifted from https://github.com/paypal/Checkout-NodeJS-SDK
+    const request2 = new paypal.orders.OrdersCreateRequest()
+    request2.headers.Prefer = 'return=representation'
+    request2.requestBody({
       intent: 'CAPTURE',
       purchase_units: [
         {
           amount: {
             currency_code: 'USD',
-            value: req.body.order_price+"",
+            value: `${body.order_price}`,
           },
         },
       ],
     })
-    const response = await PaypalClient.execute(request)
+    const response = await PaypalClient.execute(request2)
     if (response.statusCode !== 201) {
       console.log("RES: ", response)
-      return res.status(500).json({success: false, message: "Some Error Occured at backend"})
+      return NextResponse.json({ success: false, error: "Please provide order Id and Price" })
     }
 
-    ...
 
     // Your Custom Code for doing something with order
-    // Usually Store an order in the database like MongoDB
+    // Usually Store an order in the database like MongoDB   
 
-    ...   
-
-    res.status(200).json({success: true, data: {order}})
+    return NextResponse.json({response})
   } 
   catch(err){
     console.log("Err at Create Order: ", err)
-    return res.status(500).json({success: false, message: "Could Not Found the user"})
+    return NextResponse.json({ success: false, error: "No user" })
   }
 
 }
